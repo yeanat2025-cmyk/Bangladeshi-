4cc42e68635c205464a89e3bbc4883c2cdecce96
yeanat2025-cmyk <yeanat2025@gmail.com>
2026-03-10 19:41:41 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,92 @@
+const socket = io();
+
+let localStream;
+let peer;
+let recorder;
+
+const video = document.getElementById("video");
+
+/* USER JOIN CALL */
+
+async function joinCall(){
+
+socket.emit("user-call");
+
+localStream = await navigator.mediaDevices.getUserMedia({
+video:true,
+audio:true
+});
+
+video.srcObject = localStream;
+
+}
+
+/* ADMIN RECEIVE CALL */
+
+socket.on("incoming-call", ()=>{
+
+let accept = confirm("User Calling... Accept?");
+
+if(accept){
+
+startPeer();
+
+}
+
+});
+
+/* WEBRTC CONNECTION */
+
+function startPeer(){
+
+peer = new RTCPeerConnection();
+
+localStream.getTracks().forEach(track=>{
+peer.addTrack(track, localStream);
+});
+
+peer.ontrack = e=>{
+video.srcObject = e.streams[0];
+};
+
+}
+
+/* VIDEO RECORD */
+
+function startRecord(){
+
+recorder = new MediaRecorder(localStream);
+
+recorder.start();
+
+}
+
+/* STOP RECORD */
+
+function stopRecord(){
+
+recorder.stop();
+
+}
+
+/* SNAPSHOT IMAGE */
+
+function takePhoto(){
+
+let canvas = document.createElement("canvas");
+
+canvas.width = video.videoWidth;
+canvas.height = video.videoHeight;
+
+canvas.getContext("2d").drawImage(video,0,0);
+
+let img = canvas.toDataURL("image/png");
+
+let link = document.createElement("a");
+
+link.href = img;
+link.download = "snapshot.png";
+
+link.click();
+
+}o
